249b125f3dbbff1515abfb0ec496187bb9f4815f
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 01:22:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,11 +3,11 @@ numbers = [1, 2, 3]
 def square_array(array)
   count = 0
   new_numbers = Array.new
-  while count < array.length {
+  while count < array.length do
     new_numbers << (array[count] ** 2)
     count += 1
-  }
-  puts new_numbers
+  end
+  new_numbers
 end
 
 square_array(numbers)
